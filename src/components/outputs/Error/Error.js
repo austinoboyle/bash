e1be6818839594443ed2950a7cc83a387883640a
austinoboyle <14arob@queensu.ca>
2018-01-13 12:43:22 -0500

    Add propTypes

--- src/components/outputs/Error/Error.js
+++ src/components/outputs/Error/Error.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import exact from 'prop-types-exact';
 
 const Error = ({msg}) => {
     return <div className="error-msg">{msg}</div>;
 };
+
+Error.propTypes = exact({
+    msg: PropTypes.string
+});
 
 export default Error;
