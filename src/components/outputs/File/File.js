83003bead57a1d4bbc3a657b432773be355f7fce
austinoboyle <14arob@queensu.ca>
2018-01-13 12:43:36 -0500

    Add proptypes

--- src/components/outputs/File/File.js
+++ src/components/outputs/File/File.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './File.scss';
 
+import PropTypes from 'prop-types';
+import exact from 'prop-types-exact';
+
 const File = ({name, type}) => {
     return <div className={`${styles[type]}`}>{name}</div>;
 };
+
+File.propTypes = exact({
+    name: PropTypes.string,
+    type: PropTypes.string
+});
 
 export default File;
