d34e0d1cce4d7ead98f814a69a3896b8d5f144ac
austinoboyle <14arob@queensu.ca>
2018-03-18 10:57:41 -0400

    Fix linting errors - unused imports

--- src/components/Vim/Vim/Vim.js
+++ src/components/Vim/Vim/Vim.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import { goToPath } from '../../../util';
 
 import VimEditor from '../VimEditor/VimEditor';
--- src/components/Vim/VimEditor/VimEditor.js
+++ src/components/Vim/VimEditor/VimEditor.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import {quit, write} from '../../../actions/vimActions';
 
 import {getLanguageFromFilename} from '../../../util';
