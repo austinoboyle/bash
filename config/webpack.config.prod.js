72e2ed85c8e93a6f6f8431a312e39af8b76dcb9a
austinoboyle <14arob@queensu.ca>
2018-03-18 13:07:07 -0400

    Edit webpack prod config to use scss modules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.s?css$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -180,8 +180,10 @@ module.exports = {
                     {
                       loader: require.resolve('css-loader'),
                       options: {
-                        importLoaders: 1,
+                        importLoaders: 2,
                         minimize: true,
+                        modules: true,
+                        localIdentName: '[name]_[local]__[hash:base64:4]',
                         sourceMap: shouldUseSourceMap,
                       },
                     },
@@ -205,6 +207,7 @@ module.exports = {
                         ],
                       },
                     },
+                    'sass-loader'
                   ],
                 },
                 extractTextPluginOptions
