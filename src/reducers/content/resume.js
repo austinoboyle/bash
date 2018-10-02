b18f0e31e11ade1764b3af0840c1a9d32faf4a2a
austinoboyle <14arob@queensu.ca>
2018-10-02 01:06:21 -0400

    Update to most recent resume

--- src/reducers/content/resume.js
+++ src/reducers/content/resume.js
@@ -1,111 +1,56 @@
-export const resume = 
-`# AUSTIN O'BOYLE - SOFTWARE DEVELOPER
+export const resume = `Austin O’Boyle
+linkedin.com/in/austinoboyle github.com/austinoboyle austin.oboyle@queensu.ca 416-566-4957
 
-## PROFILE
+EDUCATION
+B.ScH – Physics Major, Math Minor | Queen’s University | 2014 – April 2019
+- Included 16-month professional internship. GPA: 4.0/4.3 (3.85/4.0)
 
-Software developer with 4 years of experience using Python, & 1 year of
-experience developing, testing, and deploying production web apps with React,
-NodeJS, Bluemix, and more.
-________________________________________________________________________________
+Activities: Varsity Baseball, Satellite Team, QHacks, Web Dev Club, O-Week Leader, Best Buddies
 
-## SKILLS/TOOLS
+SKILLS
+Languages/Tools: Python, Javascript, Node.js, React.js, Redux, C++, Git, MongoDB, SQL, HTML,
+CSS, SASS, Jest, Enzyme, GoogleTest, Jenkins, Webpack, GCP, AWS, Azure, Bluemix
+Skills: Data Structures, Algorithms, Full-Stack Web Development, Agile Methodology
 
-- Python
-- Javascript
-- Git
-- React/Redux
-- Jest/Enzyme
-- NodeJS
-- Gulp
-- MySQL
-- MongoDB
-- Bluemix
-- AWS
-- TravisCI
-- Webpack
-________________________________________________________________________________
+PROFESSIONAL EXPERIENCE
+Software Developer | Queen’s Centre for Advanced Computing | May 2017 – Aug 2018
+- Saved 2+ months of work on overdue project by automating task and re-developing app.
+- Helped secure team’s first contract (worth $150,000+) by building a full-stack web app
+prototype in less than 2 weeks that used Flask, KnockoutJS, and a classification neural net.
+- Developed production web app to help med students choose their discipline using Node and
+React. Wrote Jest/Enzyme tests, and created CI pipeline from GitHub → Jenkins → Azure.
+- Gave client company’s employees ability to search for documents in previously inaccessible
+databases by creating a custom IBM Watson Explorer connector and a React front-end app.
+- Created internal information-retrieval chatbot for client company using a NodeJS server,
+Watson Conversation app, Watson NLU API, and a React/Redux front-end.
 
-## PROFESSIONAL EXPERIENCE
+PROJECTS
+Thesis – Intensity Augmented SLAM | Algorithms, Robotics | Aug 2018 - Present
+- Attempting to improve existing SLAM algorithms by incorporating intensity data from LiDAR and
+ToF sensors into the observation model used to estimate sensor poses and robot trajectory.
 
-**SOFTWARE DEVELOPER**
-Queen's Centre for Advanced Computing (May 2017 - Present)
+ScrapeLI | Python, Selenium, Click, Multiprocessing, LinkedIn, Web-Scraping | Jan 2018 - Present
+- Developed Python package to turn LinkedIn pages into JSON. Includes parallelization support,
+and a CLI created using Click. Grew organically to 36 stars and 13 forks on GitHub.
 
-- Lead developer on a team of 4 interns
-- 1 month into the job, helped secure team's first large contract (worth 
-  $150000) by developing a prototype in less than 2 weeks using Flask,
-  KnockoutJS, & an SPSS classification neural network.
-- Created NodeJS server, Watson Conversation app, & React/Redux front-end for a
-  chat bot used to provide company-specific info in reponse to users' natural
-  language questions
-- Currently developing a Python module that will allow visitors to ask
-  these same questions verbally to the company’s NAO robot.
-- Created Watson Explorer connector + Node/React app enabling
-  employees to search previously inaccessible databases.
-- Developed 2 more full-stack web apps with Node, React/Redux, &
-  MongoDB. Wrote unit tests using Jest/Enzyme, & set up continuous
-  integration with an Azure Web App production environment.
-________________________________________________________________________________
+Queen’s Satellite Design Team | C++, GoogleTest | Aug 2018 - Present
+- Improving usability and maintainability of code base by developing C++ classes for EPS and
+reaction wheel subsystems, and writing GoogleTest test suites.
+- Improving new-member experience & performance by running tutorials on Git, Python, & C++.
 
-## PROJECTS
+UniVerse | NodeJs, React/Redux, Mongo, Firebase, Facebook SDK | QHacks 2018
+- Developed full stack web application that puts useful Queen’s course information in one place.
+- Contributed selenium scraper & course info* to Semester.ly GitHub project, which supports 8
+universities, ~10,000 users, and has created more than 1,000,000 schedules for students.
+* Unfortunately, Queen’s legal recently requested that their information be removed from the site.
 
-**SCRAPELI**
-- Python module to parse Linkedin profiles into json documents
-- Created command line utility installed with the module using click
+Fluid Dynamics – Design Lab | Python, Numpy, Pandas, Matplotlib, Arduino | Jan 2017
+- Explored applications of non-Newtonian fluids in body armour by analyzing accelerometer data
+of small objects moving through them. Displayed live visualizations of effects using Matplotlib.
 
-**UNI-VERSE**
-- React/Redux app with firebase backend that puts useful courseselction
-  information in one place. Allows users to rate courses/
-  profs, and connect with Facebook to see what friends have taken.
-
-**REACT UBUNTU TERMINAL**
-- Emulates basic terminal functions, using React/Redux to handle
-  user interactions. Supports document editing with an elementary
-  Vim implementation.
-
-**FLUID DYNAMICS - DESIGN LAB**
-- Used Python to connect to an Arduino/accelerometer and output
-  near-live data + visualizations of the effects of fluids on the objects
-- Created as part of a design lab to experiment with using nonnewtonian
-  fluids in body armour.
-________________________________________________________________________________
-
-## ADDITIONAL EXPERIENCE
-
-**TEACHING ASSISTANT**
-Queen's University (2016-2017)
-
-- Taught tutorials 2 nights/week for 1st year linear algebra
-- Helped struggling students in separate 1-1 sessions
-
-**SUPERVISOR & TRAINER**
-Pedalheads Bike Camps (2015-2016)
-
-- Flown to Vancouver to train BC March Break staff
-- Trained all 100+ new 2016 summer staff in Ontario with a team of 4 trainers.
-  Delivered intensive, interactive 2-day curriculum
-- Supervised a team of about 15 instructors at my site, which consistently
-  ranked as one of the best performing sites in Ontario based on parent reviews
-________________________________________________________________________________
-
-## Education
-
-**Queen's University - BScH ('14-'19)**
-- Mathematical Physics Specialization
-- Concurrent Education
-- GPA: 3.98/4.3
-________________________________________________________________________________
-
-## ACHIEVEMENTS
-
-**Susan Near Prize in Physics**
-> Top of class in core 2nd year physics courses
-
-**Walter Thumm Memorial Scholarship**
-> Awarded to an outstanding physics student with an interest in teaching
-
-**OUA Baseball rookie of the year**
-> Best 1st year baseball player in the Ontario University Athletics league
-
-**Gaels Alfie Pierce Award**
-> Top 1st year male varsity club athlete at Queen's
+ACHIEVEMENTS
+- Susan Near Prize in Physics: Top of class in core 2nd year physics courses at Queen’s.
+- Walter Thumm Scholarship: Outstanding physics student with an interest in teaching.
+- OUA Rookie of the Year: Best 1st year baseball player in Ontario University league.
+- Gaels Alfie Pierce Award: Best 1st year male varsity club athlete at Queen’s.
 `;
